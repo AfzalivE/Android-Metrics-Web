d913e51205b81bdc99caa5c665957adcc8545ff5
Afzal Najam <AfzalivE@users.noreply.github.com>
2023-10-11 12:53:07 -0400

    Allow top-level await

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,9 @@
 /** @type {import('vite').UserConfig} */
 export default {
-    base: 'Android-Metrics-Web'
+    base: 'Android-Metrics-Web',
+    esbuild: {
+        supported: {
+            'top-level-await': true //browsers can handle top-level-await features
+        },
+    }
 }
